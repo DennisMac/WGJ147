d4c325e831ff5d0b5ac6fa5913992cb1ef8a35a3
Dennis McWilliams <d12768@gmail.com>
2020-05-06 17:22:14 -0400

    start camera fly in

---
+++ Assets/CameraStart.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Cameras;
+public class CameraStart : MonoBehaviour
+{
+    Vector3 originalPosition;
+    Quaternion originalRotation;
+    public Vector3 StartLookat;
+    public FreeLookCam freeLookCam;
+    public ProtectCameraFromWallClip protectCameraFromWallClip;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPosition = transform.localPosition;
+        originalRotation = transform.localRotation;
+        transform.localPosition = new Vector3(0f, 40, 40);
+        transform.rotation = Quaternion.LookRotation(StartLookat - transform.position, Vector3.up);
+    }
+
+    float n = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = Vector3.Lerp(transform.localPosition, originalPosition, (Time.deltaTime/20f) * Mathf.Sqrt(n++) );
+        transform.rotation = Quaternion.LookRotation(StartLookat - transform.position, Vector3.up);
+        if ((transform.localPosition - originalPosition).sqrMagnitude < 0.02f)
+        {
+            transform.localPosition = originalPosition;
+            transform.localRotation = originalRotation;
+            this.enabled = false;
+            freeLookCam.enabled = true;
+            protectCameraFromWallClip.enabled = true;
+        }
+    }
+}
