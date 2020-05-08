ed100fb96cb3f04a8b1a880e2f914e3b018f36da
epersonf <epersoncmxf@gmail.com>
2020-05-07 21:45:37 -0300

    Enemies now only look at you when you're visible.

--- Assets/scripts/EnemyGun.cs
+++ Assets/scripts/EnemyGun.cs
@@ -13,6 +13,7 @@ public class EnemyGun : MonoBehaviour
     int repeats = 0;
     public GameObject projectilePrefab;
     public float range = 15;
+    public float aimSpeed = 20f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,37 +24,48 @@ public class EnemyGun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (player == null) return;
+        if (player == null || Global.PlayerCloaked || !CanLookAt(player)) return;
         Vector3 dist = (player.position + Vector3.up) - transform.position;
-        transform.rotation = Quaternion.LookRotation(dist, Vector3.up);
-        if (fireDelay < 0 && !Global.PlayerCloaked)
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dist, Vector3.up), aimSpeed * Time.deltaTime);
+        if (fireDelay < 0)
         {
-            RaycastHit hit;
-            Physics.Raycast(transform.position, (player.position - transform.position), out hit, range);
-            if (hit.collider != null)
-            {
-                if (hit.collider.gameObject.layer == 8)
-                {
-                    //if (dist.sqrMagnitude < rangeSquared)
-                    {
-                        Invoke("FireWeapon", 0);
-                        Invoke("FireWeapon", repeatDelay);
-                        Invoke("FireWeapon", repeatDelay * 2f);
-                        if (EyeballGun)
-                        {
-                            Invoke("FireWeapon", repeatDelay * 3f);
-                            Invoke("FireWeapon", repeatDelay * 4f);
-                            Invoke("FireWeapon", repeatDelay * 5f);
-                        }
-                        fireDelay = 2f;
-                    }
-                }
-            }
+            Fire();
         }
         else
         {
             fireDelay -= Time.deltaTime;
         }
+    }
+
+    public bool CanLookAt(Transform obj)
+    {
+        RaycastHit hit;
+        Physics.Raycast(transform.position, (obj.position - transform.position), out hit, range);
+        if (hit.collider != null)
+        {
+            if (hit.collider.gameObject.layer == 8)
+            {
+                //if (dist.sqrMagnitude < rangeSquared)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public void Fire()
+    {
+        Invoke("FireWeapon", 0);
+        Invoke("FireWeapon", repeatDelay);
+        Invoke("FireWeapon", repeatDelay * 2f);
+        if (EyeballGun)
+        {
+            Invoke("FireWeapon", repeatDelay * 3f);
+            Invoke("FireWeapon", repeatDelay * 4f);
+            Invoke("FireWeapon", repeatDelay * 5f);
+        }
+        fireDelay = 2f;
     }
 
     Vector3 offset = new Vector3(.1f, 0, 0); //line up with the barrel of gun
