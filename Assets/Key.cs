c5215f2a80a18fc356c9fee47169c9b6f9a7802c
Dennis McWilliams <d12768@gmail.com>
2020-05-11 17:21:53 -0400

    door requires key

---
+++ Assets/Key.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public BoxCollider exitCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        exitCollider.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            exitCollider.enabled = true;
+            Destroy(this.gameObject);
+        }
+    }
+}
